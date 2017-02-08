692a4b1bd7e9caaba6867ee62adb6505ac479710
Jeremy Felt <jeremy.felt@gmail.com>
2017-02-07 17:27:45 -0800

    Convert spaces to tabs in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,62 +1,62 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-	    stylelint: {
-		    src: [ "css/*.css" ]
-	    },
+		stylelint: {
+			src: [ "css/*.css" ]
+		},
 
-        concat: {
-            options: {
-                sourceMap: true
-            },
-            dist: {
-                src: 'css/*.css',
-                dest: 'tmp-style.css'
-            }
-        },
+		concat: {
+			options: {
+				sourceMap: true
+			},
+			dist: {
+				src: 'css/*.css',
+				dest: 'tmp-style.css'
+			}
+		},
 
-        postcss: {
-            options: {
-                map: true,
-                diff: false,
-                processors: [
-                    require( "autoprefixer" )( {
-                        browsers: [ "> 1%", "ie 8-11", "Firefox ESR" ]
-                    } )
-                ]
-            },
-            dist: {
-                src: "tmp-style.css",
-                dest: "style.css"
-            }
-        },
+		postcss: {
+			options: {
+				map: true,
+				diff: false,
+				processors: [
+					require( "autoprefixer" )( {
+						browsers: [ "> 1%", "ie 8-11", "Firefox ESR" ]
+					} )
+				]
+			},
+			dist: {
+				src: "tmp-style.css",
+				dest: "style.css"
+			}
+		},
 
-        clean: {
-            options: {
-                force: true
-            },
-            temp: [ 'tmp-style.css', 'tmp-style.css.map' ]
-        },
+		clean: {
+			options: {
+				force: true
+			},
+			temp: [ 'tmp-style.css', 'tmp-style.css.map' ]
+		},
 
-        phpcs: {
-            plugin: {
-                src: './'
-            },
-            options: {
-                bin: "vendor/bin/phpcs --extensions=php --ignore=\"*/vendor/*,*/node_modules/*\"",
-                standard: "phpcs.ruleset.xml"
-            }
-        }
+		phpcs: {
+			plugin: {
+				src: './'
+			},
+			options: {
+				bin: "vendor/bin/phpcs --extensions=php --ignore=\"*/vendor/*,*/node_modules/*\"",
+				standard: "phpcs.ruleset.xml"
+			}
+		}
 
-    });
+	});
 
-    grunt.loadNpmTasks( "grunt-postcss" );
-    grunt.loadNpmTasks( "grunt-contrib-concat" );
-    grunt.loadNpmTasks( "grunt-contrib-clean" );
-    grunt.loadNpmTasks( "grunt-phpcs" );
+	grunt.loadNpmTasks( "grunt-postcss" );
+	grunt.loadNpmTasks( "grunt-contrib-concat" );
+	grunt.loadNpmTasks( "grunt-contrib-clean" );
+	grunt.loadNpmTasks( "grunt-phpcs" );
 	grunt.loadNpmTasks( "grunt-stylelint" );
 
-    // Default task(s).
-    grunt.registerTask( 'default', [ "stylelint", "concat", "postcss", "clean" ] );
+	// Default task(s).
+	grunt.registerTask( 'default', [ "stylelint", "concat", "postcss", "clean" ] );
 };
