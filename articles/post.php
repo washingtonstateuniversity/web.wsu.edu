9ca3473cc70f195f9228665691ded22d3370baa7
Jeremy Felt <jeremy.felt@gmail.com>
2018-04-10 15:12:35 -0700

    Address several PHPCS issues with post template

--- articles/post.php
+++ articles/post.php
@@ -27,7 +27,7 @@
 		</hgroup>
 
 		<?php
-		if ( is_singular() && in_array( $post_share_placement, array( 'top', 'both' ) ) ) {
+		if ( is_singular() && in_array( $post_share_placement, array( 'top', 'both' ), true ) ) {
 			get_template_part( 'parts/share-tools' );
 		}
 		?>
@@ -47,7 +47,7 @@
 			// to that. If an option is set specifically to display excerpts, default to that. Otherwise show
 			// full content.
 			if ( $post->post_excerpt ) {
-				echo get_the_excerpt() . ' <a href="' . get_permalink() . '"><span class="excerpt-more-default">&raquo; More ...</span></a>';
+				echo wp_kses_post( get_the_excerpt() ) . ' <a href="' . esc_url( get_permalink() ) . '"><span class="excerpt-more-default">&raquo; More ...</span></a>';
 			} elseif ( strstr( $post->post_content, '<!--more-->' ) ) {
 				the_content( '<span class="content-more-default">&raquo; More ...</span>' );
 			} elseif ( 'excerpt' === spine_get_option( 'archive_content_display' ) ) {
@@ -64,7 +64,7 @@
 			<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . __( 'Pages:', 'spine' ),
-				'after' => '</div>',
+				'after'  => '</div>',
 			) );
 			?>
 		</div>
@@ -72,7 +72,7 @@
 
 	<footer class="article-footer">
 		<?php
-		if ( is_singular() && in_array( $post_share_placement, array( 'bottom', 'both' ) ) ) {
+		if ( is_singular() && in_array( $post_share_placement, array( 'bottom', 'both' ), true ) ) {
 			get_template_part( 'parts/share-tools' );
 		}
 		?>
@@ -82,7 +82,7 @@
 		echo '<dl class="categorized">';
 		echo '<dt><span class="categorized-default">Categorized</span></dt>';
 		foreach ( get_the_category() as $category ) {
-			echo '<dd><a href="' . get_category_link( $category->cat_ID ) . '">' . $category->cat_name . '</a></dd>';
+			echo '<dd><a href="' . esc_url( get_category_link( $category->cat_ID ) ) . '">' . esc_html( $category->cat_name ) . '</a></dd>';
 		}
 		echo '</dl>';
 	}
@@ -97,7 +97,7 @@
 			foreach ( $university_category_terms as $term ) {
 				$term_link = get_term_link( $term->term_id, 'wsuwp_university_category' );
 				if ( ! is_wp_error( $term_link ) ) {
-					echo '<dd><a href="' . esc_url( $term_link ) . '">' . $term->name . '</a></dd>';
+					echo '<dd><a href="' . esc_url( $term_link ) . '">' . esc_html( $term->name ) . '</a></dd>';
 				}
 			}
 			echo '</dl>';
@@ -109,7 +109,7 @@
 		echo '<dl class="tagged">';
 		echo '<dt><span class="tagged-default">Tagged</span></dt>';
 		foreach ( get_the_tags() as $tag ) {
-			echo '<dd><a href="' . get_tag_link( $tag->term_id ) . '">' . $tag->name . '</a></dd>';
+			echo '<dd><a href="' . esc_url( get_tag_link( $tag->term_id ) ) . '">' . esc_html( $tag->name ) . '</a></dd>';
 		}
 		echo '</dl>';
 	}
@@ -124,7 +124,7 @@
 			foreach ( $university_location_terms as $term ) {
 				$term_link = get_term_link( $term->term_id, 'wsuwp_university_location' );
 				if ( ! is_wp_error( $term_link ) ) {
-					echo '<dd><a href="' . esc_url( $term_link ) . '">' . $term->name . '</a></dd>';
+					echo '<dd><a href="' . esc_url( $term_link ) . '">' . esc_html( $term->name ) . '</a></dd>';
 				}
 			}
 			echo '</dl>';
@@ -142,12 +142,12 @@
 				?>
 			</div><!-- .author-avatar -->
 			<div class="author-description">
-				<h2><?php printf( __( 'About %s', 'twentytwelve' ), get_the_author() ); ?></h2>
+				<h2><?php printf( esc_html__( 'About %s', 'twentytwelve' ), get_the_author() ); ?></h2>
 				<p><?php the_author_meta( 'description' ); ?></p>
 				<?php if ( '1' === spine_get_option( 'show_author_page' ) ) : ?>
 				<div class="author-link">
 					<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-						<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'twentytwelve' ), get_the_author() ); ?>
+						<?php printf( esc_html__( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'twentytwelve' ), get_the_author() ); ?>
 					</a>
 				</div><!-- .author-link	-->
 				<?php endif; ?>
